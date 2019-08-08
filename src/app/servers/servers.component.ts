c1e6e789f4a726adf90d7c9b1063e81052001b15
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-08-08 18:46:00 -0400

    Went back to the original selector.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   // Turned this into an attribute
   // Without the brackets, it's a tag.
-  selector: '[app-servers]',
+  // selector: '[app-servers]'
+  selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
 })
