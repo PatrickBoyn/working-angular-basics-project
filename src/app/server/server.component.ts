1ee4d958a3e8af748f8460a5b2a58b3b442e2dfd
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-08-09 18:31:42 -0400

    Added an ngStyle to change based on server status.

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,6 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './server.component.html',
 })
 export class ServerComponent {
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
   serverId = 10;
   serverStatus = 'offline';
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'limegreen' : 'salmon';
+  }
 }
